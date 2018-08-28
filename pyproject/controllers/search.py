d393fe31ab0fd1947fdafde5f9b15852fbfef58c
gunooookim <rjsdn315@gmail.com>
2018-08-28 23:04:24 +0900

    다중마커 정보 출력, 왼쪽 layer 정보 출력

--- pyproject/controllers/search.py
+++ pyproject/controllers/search.py
@@ -1,24 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from flask import Blueprint, render_template, request
 import googlemaps
+import pymysql
 import numpy as np
 import pandas as pd
-
-form_dict = {'e_mail' : ['rjsdn315@gmail.com'], 
-             'phone_number' : ['010-5653-5259'],
-             'address' : ['서울시 관악구 남부순환로 161길 45'],
-             'age' : [24],
-             'sex' : ['M'],
-             'family' : [4]}
-
-member = pd.DataFrame(form_dict)
 
 gmaps = googlemaps.Client(key='AIzaSyCoLfrAJNvN7zqZpqNGby1xYuZTOzkOGf0')
 
 search = Blueprint('search', __name__, template_folder='search')
 
-park = { 0 : [1,2,3] }
-# park = [[2,'강원도','새명동','노외', '춘천시', 37.8796757, 127.7296357, '춘천도시공사', '00-131','무료','14','15','16','17','18'],[3,'강원도','야외주차장','노외', '춘천시', 37.8792, 127.7292, '춘천도시공사', '111-1241','무료','14','15','16','17','18']]
-
+park = dict()
 
 # http://localhost:5000/search/
 @search.route("/")
@@ -32,5 +24,43 @@ def searchResult():
     addr_x = addr_ll['lat']
     addr_y = addr_ll['lng']
 
-    # return render_template('search_result/search_result.html')
-    return render_template('search_result/search_result.html', address=str(address), addr_x=addr_x, addr_y=addr_y, member=dict(form_dict), park=park)
+    a = addr_x + 0.010
+    b = addr_x - 0.010
+    c = addr_y + 0.025
+    d = addr_y - 0.025
+    
+    # DB 연동 - 연결
+    conn = pymysql.connect(host='127.0.0.1',user = 'root',
+                       password='1234', db='pythondb',charset='utf8')
+    # 실행자 생성
+    cursor = conn.cursor()   
+    execute_str = "select * from parkinglot where (p_lat > " + str(b) + ") and (p_lat < " + str(a) + ") and (p_long > "+ str(d) + ") and (p_long < " + str(c)+ ")"
+
+    cursor.execute(execute_str)
+    park_data = cursor.fetchall()
+
+    # initialize park(반복될때마다 데이터가 축적되면 안되므로))
+    park = dict()
+    
+    # park를 json 형식으로 저장
+    for i in park_data:
+        park_num = 'p_' +str(i[0])
+
+        park[park_num] = {                 
+            'p_province' : i[1], 
+            'p_name' : i[2],
+            'p_type' : i[3],
+            'p_address' : i[4],
+            'p_lat' : float(i[5]),
+            'p_long' : float(i[6]),
+            'p_admit' : i[7],
+            'p_number' : i[8],
+            'p_desp' : i[9],
+            'p_date1' : i[10],
+            'p_date2' : i[11],
+            'p_date3' : i[12],
+            'p_date4' : i[13],
+            'p_date5' : i[14]
+        }
+
+    return render_template('search_result/search_result.html', address=str(address), addr_x=addr_x, addr_y=addr_y, park = park)
