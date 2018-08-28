20bc3d1aa43acf98730b44b19530e76d2f3ba8a6
wndy1 <wndy1@naver.com>
2018-08-28 16:21:26 +0900

    상세정보 부분

--- pyproject/controllers/details.py
+++ pyproject/controllers/details.py
@@ -1,5 +1,5 @@
 import pymysql
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 # db연동
 # conn = pymysql.connect( host='localhost',  port=5000, user='root',  passwd='1234', db='pythondb', charset='utf8')
 
@@ -10,9 +10,13 @@ details = Blueprint('details', __name__, template_folder='details')
 
 @details.route("/")
 def detailpage():
-    # db연동으로 해당 주차장의 날짜 리스트 불러오기
+    # db연동 -> 
     dateList = ["14일","15일","16일","17일"]
-    return render_template('details/index.html',dateList=dateList)
+    addr_x = 37.3595704
+    addr_y = 127.105399
+    # 만족도 조사
+    return render_template('details/details.html',
+    dateList=dateList, addr_x=addr_x,addr_y=addr_y)
 
 # 날짜 선택 값이 db로 저장
 # @details.route("회원관리 page", methods = ["POST"])
