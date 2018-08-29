e17e415ad15b17ff4ad31ec8d711e03c8203e35e
gunooookim <rjsdn315@gmail.com>
2018-08-29 16:08:49 +0900

    로그인 구현

--- pyproject/app.py
+++ pyproject/app.py
@@ -6,10 +6,22 @@ from controllers.details import details
 # app 객체 생성
 app = Flask(__name__)
 
-# 라우터 설정
-@app.route('/')
-def main():
-    return "main page"
+@app.route("/")
+def login():
+    return render_template('login/login.html')
+
+@app.route("/login_result", methods=['POST'])
+def login_result():
+    return render_template('login/login_result.html')
+
+@app.route("/member")
+def member():
+    return render_template('member/member.html')
+
+
+@app.route("/signup_com")
+def signup_com():
+    return render_template('login/signup_com.html')
 
 app.register_blueprint(search,url_prefix = '/search')
 app.register_blueprint(details,url_prefix = '/details')
