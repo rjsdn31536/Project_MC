0687e40fed1b533b881679d5b3b74afe6cf11454
gunooookim <rjsdn315@gmail.com>
2018-08-30 15:14:34 +0900

    회원가입 구현 및 한줄평 구현

--- pyproject/app.py
+++ pyproject/app.py
@@ -9,19 +9,50 @@ import pymysql
 app = Flask(__name__)
 app.secret_key = "e21lr1or2AKO@2rkARKM@RAR@ANK2raar,SD2"
 
+def insertData(user_email,user_pnum,user_address,user_age,user_sex,user_family):
+    # DB 연동 - 연결
+    conn = pymysql.connect(host='localhost',port=3306,user='root',passwd='1234', db='pythondb',charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+    cursor = conn.cursor()
+    sql = "insert into member (e_mail,phone_number,address,age,sex,family)\
+                     values (%s,%s,%s,%s,%s,%s);"
+    cursor.execute(sql, (user_email,user_pnum,user_address,user_age,user_sex,user_family))
+    conn.commit()
+
+    execute_str = 'select * from member where e_mail = "' + user_email + '"'
+
+    cursor.execute(execute_str)
+    member_data = cursor.fetchall()
+
+    return member_data
+
 @app.route("/")
 def login():
     return render_template('login/login.html')
 
 @app.route("/login_result", methods=['POST'])
 def login_result():
-    return render_template('login/login_result.html')
+    
+    # 폼필드 받은 값을 변수에 저장
+    user_email = request.form['email']
+    user_pnum = request.form['phone']
+    user_address = request.form['address']
+    user_age = request.form['age']
+    user_age = int(user_age)
+    user_sex = request.form['gender']
+    user_family = request.form['family']
+    user_family = int(user_family)
+    # 함수 실행
+    member_data = insertData(user_email,user_pnum,user_address,user_age,user_sex,user_family)
+
+    session['ID'] = user_email
+
+    return render_template('search/index.html', member_data=member_data)
 
 @app.route("/member")
 def member():
     # DB 연동 - 연결
     conn = pymysql.connect(host='127.0.0.1',user = 'root',
-                    password='1234', db='pythondb1',charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+                    password='1234', db='pythondb',charset='utf8', cursorclass=pymysql.cursors.DictCursor)
     # 실행자 생성
     cursor = conn.cursor()   
 
