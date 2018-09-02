91ac25d97ed858dd70dcd578ac1a3eecd8b9aad0
wndy1 <39399932+wndy1@users.noreply.github.com>
2018-09-02 23:18:17 +0900

    데이터 시각화 설명추가

--- README.md
+++ README.md
@@ -9,9 +9,29 @@
 
 4) final.csv를 parkinglot 테이블에 입력한 후, memeber.csv를 member 테이블에 입력해준다.
 
-2. pyproject 폴더를 들어가 app.py를 실행한다. 웹으로 가서 사이트 구경하길 바란다.
 
-3. 오류있으면 feedback 부탁합니다.
+
+2. 데이터 시각화
+- 공공주차장 데이터를 활용한 시각화는 두가지로 진행했는데 하나는 막대그래프, 다른 하나는 네이버 지도를 이용한 점지도를 만들었다.
+
+- 요구되는 정보는 각 시도별 공공주차장 수였다.
+
+- datalab.py를 생성, Blueprint 클래스로 app.py에 연동,db에 연동한 후 각 시도별 주차장 수 카운트 하는 find_do 함수 생성, find_do 함수를 통해 각 시도별 주차장 수 카운트, 필요한 정보 추출 후 저장, sido_list(시도별 리스트), sido_count(시도별 주차장수), sido_percent(시도별 주차장수 퍼센트), sido_addr(시도별 중심위경도 리스트), sido_json(sido_percent와 sido_addr를 json 형태로 저장)
+
+- 막대그래프
+    1) chart.js를 활용, sido_list를 label, sido_count를 data로 넣어서 막대그래프 출력하였다.
+    2) for문과 if문을 통해 data가 1000이 넘는 부분과 그렇지 않은 부분은 막대그래프 색상으로 구분지었다.
+
+- 점지도
+    1) 네이버 지도를 이용하기에 naver map api key가 필요하다.
+    2) 등록된 자신의 아이디를 html head 영역에 가져오는데 중요한 것은 점지도를 만드는 것이기에 geocoder 뒤에 drawings, visualization를 붙여야한다. 그렇지 않으면 생성이 안됀다.
+    3) new naver.maps.Map를 통해 기본 맵, 기본줌과 중심이 될 위경도 설정한다.
+    4) datalab.py에서 만든 sido_json를 data에 넣어준다.
+    5) naver.maps.visualizaiton.DotMap 인스턴스로 점지도를 출력한다.
+
+3. pyproject 폴더를 들어가 app.py를 실행한다. 웹으로 가서 사이트 구경하길 바란다.
+
+4. 오류있으면 feedback 부탁합니다.
 
 
 
--- pyproject/controllers/datalab.py
+++ pyproject/controllers/datalab.py
@@ -3,12 +3,15 @@ import googlemaps
 import pymysql
 import numpy as np
 import pandas as pd
-
+# googlemap api key
 gmaps = googlemaps.Client(key='AIzaSyCoLfrAJNvN7zqZpqNGby1xYuZTOzkOGf0')
 
-
+# Blueprint 클래스를 통한 모듈별 협업
+# datalab의 변수에 Blueprint 인스턴스 생성
 datalab = Blueprint('datalab', __name__, template_folder='dataLab')
 
+# find_do 함수 생성
+# db연동 후  execute_str문을 통해 각 도별 주차장수 카운트
 def find_do(do):
     conn = pymysql.connect(host='mc-project.crzhz77savee.ap-northeast-2.rds.amazonaws.com',port=3306,user='mc_project',passwd='multicampus', db='pythondb',charset='utf8', cursorclass=pymysql.cursors.DictCursor)
     cursor = conn.cursor()
@@ -19,29 +22,35 @@ def find_do(do):
     conn.close()
     return num['cnt']
 
-
+# datalab 라우터 설정
 @datalab.route('/')
 def dataLab():
     # 막대 그래프
     sido = ['전라남도', '울산광역시', '충청북도','강원도', '대구광역시', '광주광역시', '경상남도', '인천광역시', '부산광역시', '대전광역시', '서울특별시', '충청남도', '제주특별자치도', '세종특별자치시', '서울특별시', '경상북도', '전라북도', '경기도']
-    sidoset = set(sido)
-    sido_list = list(sidoset)
+    sidoset = set(sido) # sido의 중복리스트를 없애기 위해 집합 변환
+    sido_list = list(sidoset) # 다시 sido_list로 리스트 변환
 
+    # 시도별 주차장 수 카운트 후 sido_count 리스트로 저장
     sido_count = []
     for i in sido_list:
         a = find_do(i)
         sido_count.append(a)
     
     # 점지도를 위해 json 형태로 만들기
+
+    # 시도별 점의 가중치를 주기위해 동일한 비율로 변환
     sido_percent = []
     for i in sido_count:
         a = (i/sum(sido_count))*100
         sido_percent.append(a)
+    
+    # 구글을 통해 각 시도별 중심 위경도를 sido_addr 리스트로 저장
     sido_addr = []
     for i in sido_list:
         address = i
         addr = gmaps.geocode(address, language='ko')[0]['geometry']['location']
         sido_addr.append(addr)
+    # sido_percent와 sido_addr리스트를 sido_json에 json의 형태로 저장
     sido_json = []
     for i in range(len(sido_list)):
         x = { "weight": sido_percent[i], "location": sido_addr[i] }
